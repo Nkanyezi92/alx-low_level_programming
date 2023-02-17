3fd2bbf70481fd7e55c208b4bb06bb4d74de9f18
Nkanyezi92 <Nkanyezink@gmail.com>
2023-02-17 02:53:25 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@
 int main(void)
 
 {
-	int n;
+                	int n;
 
-	srand(time(0)0;
+                        srand(time(0));
 			n = rand() - RAND_MAX / 2;
 
 			if (n > 0)
@@ -33,5 +33,5 @@ int main(void)
 			}
 			return (0); 
 			
-			}
+}
 
