863d27adfe9f255c9b30c2b63e230266e1adf83c
Nkanyezi92 <Nkanyezink@gmail.com>
2023-02-22 23:23:09 -0800

    2 Muktiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @x: - second integer 
+ * @y: firsr integer 
+ * Return: Result of x multiplied y
+ */
+
+int mul(int x, int y)
+{
+	return(x * y);
+
+}
+
+
